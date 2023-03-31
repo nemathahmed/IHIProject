bea5537a08f0e5e93d494cf65988668b93328822
Amruta Rao <amurao2008@gmail.com>
2023-03-31 17:42:04 -0400

    added category for patient and provider page to homepage

--- homePage.js
+++ homePage.js
@@ -2,7 +2,17 @@ const icon = document.getElementById('icon');
 icon.src = 'icon.png';
 
 const button = document.getElementById('get-started-button');
+const buttonsContainer = document.getElementById('category-button-container');
 button.addEventListener('click', () => {
-    // Do something when the button is clicked
-    console.log('Button clicked!');
+    buttonsContainer.style.display = 'block';
+    button.style.display = 'none';
 });
+
+const patientButton = document.getElementById('patient-button');
+patientButton.addEventListener('click', () => {
+    console.log('patient button is clicked');
+})
+const providerButton = document.getElementById('provider-button');
+providerButton.addEventListener('click', () => {
+    console.log('provider button is clicked');
+})
