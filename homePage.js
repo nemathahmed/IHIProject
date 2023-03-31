929056d441062520b351cfac899253db245b4a50
Amruta Rao <amurao2008@gmail.com>
2023-03-30 21:31:57 -0400

    added homepage files on the frontend

---
+++ homePage.js
@@ -0,0 +1,8 @@
+const icon = document.getElementById('icon');
+icon.src = 'icon.png';
+
+const button = document.getElementById('get-started-button');
+button.addEventListener('click', () => {
+    // Do something when the button is clicked
+    console.log('Button clicked!');
+});
